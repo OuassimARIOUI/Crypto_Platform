8832876da00b97ff6ab825767c1ab12b9e4c65d5
Wassim991 <mohamed-achraf-ouassim.arioui@alumni.univ-avignon.fr>
2025-11-04 19:08:42 +0100

    Test unit des services de la BD

---
+++ backend/src/test/dbService.test.js
@@ -0,0 +1,43 @@
+import {vi, describe, test, expect} from "vitest";
+
+
+
+
+vi.mock("pg", () => {
+    const connect = vi.fn(); // simule la connexion à la DB
+    const query = vi.fn();   // simule les requetes SQL
+
+    const Client = vi.fn(function () {
+        this.connect = connect;
+        this.query = query;
+    });
+    return {default : {Client} };
+});
+
+vi.mock("../utils/logger.js", () => ({
+    logInfo: vi.fn(),
+    logError: vi.fn(),
+}));
+
+import {connectDB} from "../services/dbService.js";
+import { logInfo, logError } from "../utils/logger.js";
+
+
+describe("----DB TEST---", () => {
+    test("Connextion reussie a la DB ", async () => {
+        const {default : {Client}} = await import("pg");
+        const mockClient = new Client();
+        mockClient.connect.mockResolvedValueOnce({});
+        await connectDB();
+        expect(logInfo).toHaveBeenCalledWith(expect.stringContaining("Connexion "));
+    });
+        test("returns error if there is no connection to db", async () => {
+        const { default: { Client } } = await import("pg");
+        Client.mockImplementation(() => ({
+            connect: vi.fn().mockRejectedValueOnce(new Error("Echec Connection")),
+            query: vi.fn(),
+        }));
+        await connectDB();
+        expect(logError);
+    })
+});

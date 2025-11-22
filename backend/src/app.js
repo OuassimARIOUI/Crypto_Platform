44ac5c578cf93ecd42e950f71e2713f5ff35326d
Wassim991 <mohamed-achraf-ouassim.arioui@alumni.univ-avignon.fr>
2025-11-22 23:10:25 +0100

    integrating login and register

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cryptosRouter from "./routes/cryptos.routes.js";
+import pricesRouter from "./routes/prices.routes.js";
+import alertsRoutes from "./routes/alerts.routes.js";
+import indicatorsRoutes from "./routes/indicators.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+app.use(express.json());
+
+// Montage de la route /cryptos
+app.use("/cryptos", cryptosRouter);
+app.use("/prices",pricesRouter);
+app.use("/alerts", alertsRoutes);
+app.use("/indicators", indicatorsRoutes);
+app.use("/auth", authRoutes);
+
+export default app;
